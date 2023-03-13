e6aa7a0e462c5ba42d2c70d6718dd211245bce4b
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-03-13 18:28:33 +0100

    Pulling old MongoDB image and connecting to it

--- src/app.js
+++ src/app.js
@@ -1,12 +1,33 @@
 const express = require("express");
+const mongoose = require("mongoose")
+
+const DB_URL = process.env.ME_CONFIG_MONGODB_URL
+const PORT = process.env.PORT || 3000;
+
 
 const app = express();
+
+mongoose.connect(DB_URL)
+const db = mongoose.connection
+
+db.on('error', function(err) {
+  console.error('connection error', err);
+});
+
+db.on('connected', function() {
+    console.log('MongoDB: successfully connected');
+});
+
+db.on('disconnected', function() {
+    console.log('MongoDB: disconnected');
+});
+
+
 
 app.use("/", function (req, res) {
   res.send("Hello, World!");
 });
 
-const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function () {
   console.log(`Project is running on http://localhost:${PORT}`);
