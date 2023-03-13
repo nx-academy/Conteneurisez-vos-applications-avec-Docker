921eb408b88b0ebd9ea71f57189c23be2ab0ed9d
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-03-13 17:54:17 +0100

    Installing Express and getting Hello World

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-function main() {
-  console.log("Hello, world!")
-}
+const express = require("express");
 
-main()
+const app = express();
 
+app.use("/", function (req, res) {
+  res.send("Hello, World!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+  console.log(`Project is running on http://localhost:${PORT}`);
+});
