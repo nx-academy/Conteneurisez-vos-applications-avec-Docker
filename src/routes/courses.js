83eabfa4115edcf8e090eb7b1dcc75aaa61f1289
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-03-20 17:30:12 +0100

    Creating the user logic:
    - Routing on /course and /user
    - ORMing data
    - Creating user on POST
    - Fetching all users on GET

---
+++ src/routes/courses.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const express = require('express')
+const router = express.Router()
+
+router.get('/', function(req, res, next) {
+  res.send("Hello, courses")
+})
+
+module.exports = router
+
