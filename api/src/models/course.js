495ec726a8c50da3e3a0910aca6d03807f1d3a8f
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-04-17 10:50:09 +0200

    Adding events when realizing actions on DB

--- api/src/models/course.js
+++ api/src/models/course.js
@@ -41,6 +41,39 @@ const CourseSchema = new mongoose.Schema({
   }]
 })
 
+// Set the step number property to the correct format
+CourseSchema
+  .pre('save', function(next) {
+    this.steps = this.steps.map(function(step, index) {
+      step.stepNumber = index + 1
+      return step
+    })
+    return next()
+  })
+
+// Check if a course has at least one step
+CourseSchema
+  .path('steps')
+  .validate(function(steps) {
+    return steps.length >= 1
+  },
+  'Each course must have at least one step.')
+
+// Get an overal rating for the course
+CourseSchema
+  .virtual('overallRating')
+  .get(function() {
+    let numberOfReviews = this.reviews.length
+    let totalScore = 0
+
+    this.reviews.forEach(function(review) {
+      totalScore += review.rating
+    })
+
+    return Math.round(totalScore / numberOfReviews)
+  })
+
+
 const Course = mongoose.model("Course", CourseSchema)
 module.exports = Course
 
