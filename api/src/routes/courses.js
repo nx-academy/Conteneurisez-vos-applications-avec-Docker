9e0e2cb4ef8a0e853dd547745c62455ab8533a70
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-04-17 11:05:57 +0200

    Getting courses

--- api/src/routes/courses.js
+++ api/src/routes/courses.js
@@ -3,8 +3,22 @@
 const express = require("express");
 const router = express.Router();
 
+const Course = require("../models/course")
+const Review = require("../models/review")
+
+
 router.get("/", function (req, res, next) {
-  res.send("Hello, courses");
+  Course
+    .find()
+    .select("_id, title")
+    .exec(function(err, courses) {
+      if (err) {
+        return next(err)
+      }
+      return res.status(200).json({ courses })
+    })
 });
 
+
 module.exports = router;
+
