758ed81b9afffb91c47802cb0c41b58385b55e79
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-04-19 17:20:34 +0200

    Adding the solution

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
-const port = process.env.PORT || 3000
+
+const PORT = process.env.PORT || 3000
+const DB_URL = process.env.ME_CONFIG_MONGODB_URL
+
+mongoose.connect(DB_URL);
+const db = mongoose.connection;
+
+db.on("error", function (err) {
+  console.error("connection error", err);
+});
+
+db.on("connected", function () {
+  console.log("MongoDB: successfully connected");
+});
+
+db.on("disconnected", function () {
+  console.log("MongoDB: disconnected");
+});
+
 
 app.get('/', (req, res) => {
   res.send("Hello, world!")
 })
 
-app.listen(port, () => {
-  console.log(`App is listening on port ${port}`)
+
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}`)
 })
+
