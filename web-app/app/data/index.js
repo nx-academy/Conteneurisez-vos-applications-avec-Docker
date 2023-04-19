020b38860641b524fe4d180d52938a49fd36156a
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-04-19 15:38:36 +0200

    Cleaning main branch

--- web-app/app/data/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var angular = require('angular');
-var app = angular.module('app');
-
-app.service('coursesData', require('./courses'));
-app.service('usersData', require('./users'));
