1f463957bcb1829edd421bc4a3712a2f758107fc
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-10-17 11:58:40 +0200

    Removing the solution

--- web/src/App.js
+++ web/src/App.js
@@ -5,18 +5,7 @@ import './App.css';
 function App() {
   const [greetings, setGreetings]  = useState('world')
 
-  const API_URL = process.env.REACT_APP_API_URL
-
-  useEffect(() => {
-    function getGreetings() {
-      return fetch(`${API_URL}/greetings`)
-        .then(data => data.json())
-        .then(data => setGreetings(data.greetings))
-        .catch(err => console.log('An error occured', err))
-    }
-
-    getGreetings()
-  }, [])
+  // Vous devez faire ici un call http auprès de l'API.
 
   return (
     <div className="App">
